9cbd7d2c716d896373f9199b09fcfa82a1e8627e
Jayson San Agustin <santiagomvp72F@gmail.com>
2020-04-08 12:51:01 +0800

    feat(shared): added drag-and-drop directive

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,18 +3,19 @@ import { CommonModule } from "@angular/common";
 
 import { LoaderComponent } from "./components/loader/loader.component";
 import { StorageService } from "./service/storage.service";
+import { DndDirective } from './directive/dnd.directive';
 
 @NgModule({
-   declarations: [LoaderComponent],
+   declarations: [LoaderComponent, DndDirective],
    imports: [CommonModule],
    providers: [StorageService],
-   exports: [LoaderComponent]
+   exports: [LoaderComponent, DndDirective],
 })
 export class SharedModule {
    static forRoot(): ModuleWithProviders {
       return {
          ngModule: SharedModule,
-         providers: [[StorageService]]
+         providers: [[StorageService]],
       };
    }
 }
